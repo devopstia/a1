51957a9a8df752bba8b7621f3113b295e39249a8
Merluise Tia <Tia Leo>
2023-12-14 21:34:24 -0600

    jenkins

--- jenkins/scripts/user-management-packages.sh
+++ jenkins/scripts/user-management-packages.sh
@@ -7,6 +7,11 @@
 # amazon-ec2 = Amazon Linux
 
 PACKAGES=$1
+USERNAME=$2
+PASSWORD=$3
+EMAIL=$4
+FIRSTNAME=$5
+LASTNAME=$6
 
 OS_NAME=$(cat /etc/*release |grep -w NAME |awk -F'"' '{print$2}')
 
@@ -30,3 +35,52 @@ elif [ "$OS_NAME" == "Red Hat Enterprise Linux" ] || [ "$OS_NAME" == "CentOS Lin
 else
     echo "HUMMMM. I don't know this OS."
 fi
+
+
+echo "Username: ${params.USERNAME}"
+echo "Password : ${params.PASSWORD}"
+echo "Email: ${params.EMAIL}"
+echo "First Name: ${params.FIRSTNAME}"
+echo "Last Name: ${params.LASTNAME}"
+
+manage_user_account() {
+    if [ "$1" == "add_user" ]; then
+        if ! grep -q "^$2:" /etc/passwd; then
+            sudo useradd -m "$2"
+            echo "$2:$3" | sudo chpasswd
+            sudo usermod -c "$4 $5" "$2"
+            sudo cat /etc/passwd | grep "$2"
+            sudo cat /etc/shadow | grep "$2"
+            ls /home
+        else
+            echo "User $2 already exists"
+        fi
+    elif [ "$1" == "delete_user" ]; then
+        if ! grep -q "^$2:" /etc/passwd; then
+            echo "User $2 does not exist"
+        else
+            sudo userdel -r "$2"
+            echo "The user $2 with password $3 has been deleted"
+            sudo cat /etc/passwd | grep "$2" || true
+            sudo cat /etc/shadow | grep "$2" || true
+        fi
+    elif [ "$1" == "lock_user" ]; then
+        if ! grep -q "^$2:" /etc/passwd; then
+            echo "User $2 does not exist"
+        else
+            sudo passwd -l "$2"
+            echo "The user $2 with password $3 has been locked"
+            sudo cat /etc/shadow | grep "$2"
+        fi
+    elif [ "$1" == "unlock_user" ]; then
+        if ! grep -q "^$2:" /etc/passwd; then
+            echo "User $2 does not exist"
+        else
+            sudo passwd -u "$2"
+            echo "The user $2 with password $3 has been unlocked"
+            sudo cat /etc/shadow | grep "$2"
+        fi
+    else
+        echo "Invalid ACCOUNT_MANAGEMENT action: $1"
+    fi
+}
